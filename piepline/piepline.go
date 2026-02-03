70d63d3f4be4540d7966fb2bce058637f1a29233
BitCoinOffical <jlucehok228@gmail.com>
2026-02-03 22:06:43 +0500

    update piepline pattern

--- piepline/piepline.go
+++ piepline/piepline.go
@@ -1,27 +1,75 @@
 package piepline
 
-func Generate(values ...int) chan int {
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	in := generateWork([]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
+
+	out := filterOdd(in)
+	out = square(out)
+	out = half(out)
+
+	for v := range out {
+		fmt.Println(v)
+	}
+
+}
+
+func filterOdd(in <-chan int) <-chan int {
 	out := make(chan int)
 
 	go func() {
 		defer close(out)
-		for i := range values {
-			out <- i
+		for i := range in {
+			if i%2 == 0 {
+				out <- i
+			}
 		}
 	}()
 
 	return out
 }
 
-func PiePline(in chan int, f func(int) int) chan int {
+func square(in <-chan int) <-chan int {
 	out := make(chan int)
 
 	go func() {
 		defer close(out)
 		for i := range in {
-			out <- f(i)
+			val := math.Pow(float64(i), 2)
+			out <- int(val)
 		}
 	}()
 
 	return out
+}
+
+func half(in <-chan int) <-chan int {
+	out := make(chan int)
+
+	go func() {
+		defer close(out)
+		for i := range in {
+			val := i / 2
+			out <- val
+		}
+	}()
+
+	return out
+}
+
+func generateWork(work []int) <-chan int {
+	ch := make(chan int)
+
+	go func() {
+		defer close(ch)
+		for _, w := range work {
+			ch <- w
+		}
+	}()
+
+	return ch
 }
